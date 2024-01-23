9383232385d324d491a1e9aa62ebbe5ffcf35b3c
min-38 <mindino03@gmail.com>
2024-01-23 18:24:21 +0900

    Tailwind CSS 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './main.css';
 import Home from "./pages/Home";
 
 function App() {
 
   return (
+    <div className="grid gap-6 mb-6 md:grid-cols-2">
       <Home />
+    </div>
   )
 }
 
